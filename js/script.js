6f5edfe9515ef6c100b50746d560080abcfba4f9
BudiSalah <budi.salah@outlook.com>
2020-02-29 15:54:49 +0200

    Finish the api and the task

--- js/script.js
+++ js/script.js
@@ -1,54 +1,88 @@
+// global variables
+let departureInput = jQuery("#departure"),
+    arrivalInput = jQuery("#arrival");
+
 jQuery(document).ready(() => {
-    let departureInput = jQuery("#departure"),
-        arrivalInput = jQuery("#arrival"),
-        departureOutput = jQuery(".bu_departure-output")[0],
-        arrivalOutput = jQuery(".bu_arrival-output")[0];
-
-    departureInput.keydown(e => {
-        let param = e.target.value;
-            
-        if (param.length > 1 && param != " ") {
-            let allData = get_data(param);
-
-            allData.then(datas => {
-                console.log("datas");
-                console.log(datas.length);
-
-                for (data of datas) {
-                    set_item(departureOutput, data);
-                }
-            });
-        }
-    });
+    departureInput.on("keyup click", event_trigger);
+    arrivalInput.on("keyup click", event_trigger);
 });
 
-async function get_data(param) {
-    let response = await fetch(`https://booking-engine.fare33.com/api/airport/autocomplete?term=${param}`);
-    let datas = await response.json();
+function event_trigger(e) {
+    let param = e.target.value,
+        outputTarget;
+        
+    if (param.length > 1 && param != " ") {
+        if (jQuery(e.target).is("#departure")) {
+            outputTarget = jQuery(".bu_departure-output")[0];
+        } else if (jQuery(e.target).is("#arrival")) {
+            outputTarget = jQuery(".bu_arrival-output")[0];
+        };
 
-    return datas;
+        jQuery(departureInput).focus(() => {
+            jQuery(".bu_arrival-output").css("display", "none");
+        });
+        
+        jQuery(arrivalInput).focus(() => {
+            jQuery(".bu_departure-output").css("display", "none");
+        });
+
+        data_output(param, outputTarget);
+    };
+}
+
+async function data_output(param, outputContainer) {
+    await fetch(`https://booking-engine.fare33.com/api/airport/autocomplete?term=${param}`)
+    .then(async response => response.json())
+    .then(datas => {
+        jQuery(outputContainer).empty();
+        jQuery(outputContainer).css("display", "none");
+
+        if (datas.length > 0) {
+            for (data of datas) {
+                set_item(outputContainer, data);
+            };
+
+            jQuery(outputContainer).css("display", "block");
+            select_data(outputContainer);
+            hide_output(outputContainer);
+        } else {
+            jQuery(outputContainer).css("display", "none");
+            return 0;
+        }
+    });
 };
 
-function set_item(parent, data) {
+function set_item(parentOutput, data) {
     let newLiElement = document.createElement("li"),
-        parentChilds = jQuery(parent).children(),
-        {City} = data;
+        {Name, Country} = data;
 
-    console.log("parentChilds.length")
-    console.log(parentChilds.length)
+    newLiElement.append(`${Name}, ${Country}`);
 
-    for (child of parentChilds) {
-        if (child.innerHTML == City) {
-            return 0;
-        };
+    if (jQuery(parentOutput).is(".bu_departure-output")) {
+        jQuery(parentOutput).append(newLiElement);
+    } else if (jQuery(parentOutput).is(".bu_arrival-output")) {
+        jQuery(parentOutput).append(newLiElement);
     };
+};
 
-    newLiElement.append(data.City);
-
-    if (jQuery(parent).is(".bu_departure-output")) {
-        jQuery(parent).append(newLiElement);
-    } else if ((jQuery(parent).is(".bu_arrival-output"))) {
-        jQuery(parent).append(newLiElement);
-    };
+function select_data(outputContainer) {
+    let parentChilds = jQuery(outputContainer).children();
     
+    parentChilds.click(e => {
+        if (jQuery(outputContainer).is(".bu_departure-output")) {
+            departureInput[0].value = e.currentTarget.innerText;
+        } else if (jQuery(outputContainer).is(".bu_arrival-output")) {
+            arrivalInput[0].value = e.currentTarget.innerText;
+        };
+        
+        jQuery(outputContainer).css("display", "none");
+    });
+};
+
+function hide_output(outputContainer) {
+    let allElements = jQuery("*");
+
+    allElements.click(e => {
+        jQuery(outputContainer).css("display", "none");
+    });
 };
